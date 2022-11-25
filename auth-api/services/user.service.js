e82ceed1e7fb3540e74451a1fa72a0c38b1a015e
atetheone <atevirran@gmail.com>
2022-11-25 23:34:12 +0100

    user services updated

--- auth-api/services/user.service.js
+++ auth-api/services/user.service.js
@@ -1,27 +1,50 @@
-const bcrypt = require('bcrypt');
-const User = require('../models/user.schema.js');
+const bcrypt = require("bcrypt");
+const User = require("../models/user.schema.js");
+const jwt = require("jsonwebtoken");
 
-const login = async (email, pass) => {
-  // return await User.findOne();
+const login = async (email, password) => {
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      // email not found
+      return { error: "CREDENTIALS_INVALID", msg: "Email/Password invalid" };
+    }
+    // user found
+    const validPassword = await bcrypt.compare(password, user.hash);
+
+    if (!validPassword) {
+      // invalid password
+      return { error: "CREDENTIALS_INVALID", msg: "Email/Password invalid" };
+    }
+    //password valid
+    const JWT_SECRET = process.env.JWT_SECRET;
+    const token = await jwt.sign({ sub: user._id, username: user.email }, JWT_SECRET, {
+      algorithm: 'HS256',
+      expiresIn: '1d'
+    });
+    return {token, ...user._doc};
+  } catch (e) {
+    console.log({ error: e });
+  }
 };
 
 const signup = async (name, email, pass) => {
   let user;
   try {
-    const userFound = await User.findOne({email: email });
+    const userFound = await User.findOne({ email });
     if (userFound) {
-      throw Error('User already in DB');
+      return { error: "OPERATION_FORBIDDEN_ERROR", msg: "User already in DB" };
     }
-    const hash = bcrypt.hash(pass, 12);
+    const hash = await bcrypt.hash(pass, 12);
     user = User({
       name,
       email,
-      hash
+      hash,
     });
 
     await user.save();
   } catch (e) {
-    console.error({error: e});
+    console.error({ error: e });
   }
 
   return user;
@@ -39,5 +62,5 @@ module.exports = {
   login,
   signup,
   getUsers,
-  getProfile
-}
+  getProfile,
+};
