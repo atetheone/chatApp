3af419c78aa6f9567a5e23a393e8293464070ef4
atetheone <atevirran@gmail.com>
2022-12-02 15:24:28 +0100

    login updated

--- chat-ui/src/app/components/login/login.component.ts
+++ chat-ui/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
+  
+  loginForm: FormGroup = this.fb.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
   }
+
+  get password() {
+    return this.loginForm.get('password')?.value;
+  }
+
+  get email() {
+    return this.loginForm.get('email')?.value;
+  }
+
+
+  login() {
+    const userAuth = {
+      email: this.email,
+      password: this.password
+    };
+
+    this.authService.login(userAuth);
+  }
+
 
 }
