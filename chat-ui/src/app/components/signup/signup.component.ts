2a991506b8ff7d9c66a16f70e84d69ea554e9e3e
atetheone <atevirran@gmail.com>
2022-12-02 15:25:20 +0100

    snackbar for message popup

--- chat-ui/src/app/components/signup/signup.component.ts
+++ chat-ui/src/app/components/signup/signup.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import User from 'src/app/entities/user.entity';
 import { AuthService } from 'src/app/services/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-signup',
@@ -11,45 +13,96 @@ import { AuthService } from 'src/app/services/auth.service';
 export class SignupComponent implements OnInit {
 
   signupForm: FormGroup = this.fb.group({
-    name: ['', Validators.required],
-    email: ['', Validators.required],
-    password: ['', Validators.required],
-    cpassword: ['', Validators.required]
+    name: ['', [Validators.required, Validators.maxLength(200)]],
+    email: ['', [Validators.required, Validators.email, Validators.maxLength(200)]],
+    password: ['', [Validators.required, this.createPasswordStrengthValidator()]],
+    cpassword: ['', [Validators.required, this.passwordConfirmValidator()]]
   });
 
   constructor(
     private authService: AuthService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private router: Router,
+    private _snackBar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
   }
 
   get password() {
-    return this.signupForm.get('password')?.value;
+    return this.signupForm.get('password');
   }
 
   get cpassword() {
-    return this.signupForm.get('cpassword')?.value;
+    return this.signupForm.get('cpassword');
   }
 
   get name() {
-    return this.signupForm.get('name')?.value;
+    return this.signupForm.get('name');
   }
 
   get email() {
-    return this.signupForm.get('email')?.value;
+    return this.signupForm.get('email');
   }
 
 
-  signup() {
+  createPasswordStrengthValidator(): ValidatorFn {
+    return (control: AbstractControl) : ValidationErrors | null => {
+
+        const value = control.value;
+
+        if (!value) {
+            return null;
+        }
+
+        const hasUpperCase = /[A-Z]+/.test(value);
+        const hasLowerCase = /[a-z]+/.test(value);
+        const hasNumeric = /[0-9]+/.test(value);
+
+        const passwordValid = hasUpperCase && hasLowerCase && hasNumeric;
+
+        return !passwordValid ? {passwordStrength:true}: null;
+    }
+  }
+
+  passwordConfirmValidator(): ValidatorFn {
+    return (control: AbstractControl) : ValidationErrors | null => {
+
+        const passValue = this.signupForm?.get('password')?.value, confValue = control.value;
+
+        const passwordValid = passValue === confValue;
+
+        return !passwordValid ? {passwordCorfirmed:true}: null;
+    }
+  }
+
+
+
+
+  onSignup() {
     const user: User = {
-      name: this.name,
-      email: this.email,
-      password: this.password
+      name: this.name?.value,
+      email: this.email?.value,
+      password: this.password?.value
     };
 
-    this.authService.signup(user);
+    this.authService.signup(user)
+      .subscribe(
+        data => {
+          console.log(data);
+          this._snackBar.open("Your account has been created successfully", "", {
+            duration: 2000,
+            // here specify the position
+            verticalPosition: 'top',
+            horizontalPosition: 'right',
+            panelClass: ['bg-success']
+          })
+          this.router.navigate(['/login']);
+        },
+        error => {
+          console.error(error)
+        }
+      );
   }
 
 }
