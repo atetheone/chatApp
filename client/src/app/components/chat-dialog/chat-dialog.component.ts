75a1ca523c8bb0d1890b2e2a4546a2ec13e0bb88
atetheone <atevirran@gmail.com>
2022-12-13 14:05:19 +0100

    generate chat-dialog component

---
+++ client/src/app/components/chat-dialog/chat-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-dialog',
+  templateUrl: './chat-dialog.component.html',
+  styleUrls: ['./chat-dialog.component.sass']
+})
+export class ChatDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
