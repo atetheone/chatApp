fe0c2c592f3eff9291c75a15bc50ff12027f471e
atetheone <atevirran@gmail.com>
2022-12-13 15:10:13 +0100

    generate chat-list-users component

---
+++ client/src/app/components/chat-list-users/chat-list-users.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-list-users',
+  templateUrl: './chat-list-users.component.html',
+  styleUrls: ['./chat-list-users.component.sass']
+})
+export class ChatListUsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
