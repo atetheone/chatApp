e1d01abd8538b6082ca517d40c9ce532befd5f0c
atetheone <atevirran@gmail.com>
2022-12-28 11:31:14 +0100

    update errors

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,9 +1,18 @@
+import { CustomError } from "./custom-error";
 
-export class DatabaseConnectionError extends Error {
-	reasons = "Error connecting to the database";
+export class DatabaseConnectionError extends CustomError {
+	reason = "Error connecting to the database";
+	statusCode = 500;
+
   constructor() {
-    super();
+    super("Error connecting to the DB");
 
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
   }
+
+	serializeErrors() {
+		return [
+			{ message: this.reason }
+		]
+	}
 }
--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,11 +1,19 @@
 import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
+  statusCode = 400;
   constructor(
-    private errors: ValidationError[]
+    public errors: ValidationError[]
   ) {
-    super();
+    super("Error validating request");
 
     Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map(error => {
+      return { message: error.msg, field: error.param }
+    });
   }
 }
