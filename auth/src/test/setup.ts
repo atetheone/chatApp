7f4c7ae126059fa82f27df3c49cea8fd3c4b70f1
atetheone <atevirran@gmail.com>
2023-01-02 15:26:40 +0100

    setup testing with jest

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,24 @@
+import app from "../app";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+
+let mongo: MongoMemoryServer;
+beforeAll(async () => {
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+}); 
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+	collections.forEach(async collection => {
+		await collection.deleteMany({});
+	});
+});
+
+afterAll(async () => {
+	await mongo.stop();
+	await mongoose.connection.close();
+})
